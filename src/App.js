3410eea919722aa948bfd1004267676d7c031886
JUHEEBAEK <bjh9287@naver.com>
2021-07-05 22:25:01 +0900

    useState를 통해 컴포넌트에서 바뀌는 값 관리

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from "react";
-import Hello from "./Hello";
-import Wrapper from "./Wrapper";
+import Counter from "./counter";
 
 function App() {
   return (
-    <Wrapper>
-      <Hello name="react" color="red" isSpecial={true} /> {/* isSpecial 요렇게만 써도 됨. */}
-      <Hello color="pink" />
-    </Wrapper>
+      <Counter></Counter>
   );
 }
 
