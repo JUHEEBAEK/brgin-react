fada33c726ac8373be4dc80816e4d4640f0f66e9
JUHEEBAEK <bjh9287@naver.com>
2021-07-11 22:47:20 +0900

    배열 렌더링 하기

--- src/UserList.js
+++ src/UserList.js
@@ -30,9 +30,12 @@ function UserList() {
 
     return (
         <div>
-            <User user={users[0]} />
-            <User user={users[1]} />
-            <User user={users[2]} />
+            {
+                users.map(
+                    user => (<User user={user} key={user.id} />) 
+                    //key 없을 경우 index 사용. 그러나 key값으로 index를 사용하는게 효율적이진 않다.(효과없음.)
+                )
+            }
         </div>
     )
 
