acf894d0fb9fcdf4b2e0c93e062cc50c6c90dfe2
김철 <dmsghs92@gmail.com>
2022-03-24 11:40:05 +0900

    [feat] Props에 리스트 넘기고 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
 
 import './App.css';
-import {Greet } from "./components/Greet"
+import { Greet } from "./components/Greet"
+import { Person } from "./components/Person"
+import { PersonList } from './components/PersonList';
 
 
 function App() {
+  const personName = {
+    first: 'Bruce',
+    last: 'James'
+  }
+  const nameList = [
+    {
+      first: 'Bruce',
+      last: 'Wayne'
+    },
+    {
+      first: 'Clark',
+      last: 'Kent'
+    },
+    {
+      first: 'Princess',
+      last: 'Diana'
+    },
+  ]
   return (
     <div className="App">
       <Greet name='cheol' messageCount={10} isLoggedIn={true} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   );
 }
